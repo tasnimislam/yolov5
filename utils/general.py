5d3777a2b9c0a464cd8570c8488ff1f8c0c6af1a
Tasnim Nishat Islam <46325413+tasnimislam@users.noreply.github.com>
2020-11-24 10:32:44 +0600

    added increment path

--- utils/general.py
+++ utils/general.py
@@ -27,6 +27,20 @@ from tqdm import tqdm
 from utils.google_utils import gsutil_getsize
 from utils.torch_utils import is_parallel, init_torch_seeds
 
+
+def increment_path(path, exist_ok=True, sep=''):
+    # Increment path, i.e. runs/exp --> runs/exp{sep}0, runs/exp{sep}1 etc.
+    path = Path(path)  # os-agnostic
+    if (path.exists() and exist_ok) or (not path.exists()):
+        return str(path)
+    else:
+        dirs = glob.glob(f"{path}{sep}*")  # similar paths
+        matches = [re.search(rf"%s{sep}(\d+)" % path.stem, d) for d in dirs]
+        i = [int(m.groups()[0]) for m in matches if m]  # indices
+        n = max(i) + 1 if i else 2  # increment number
+        return f"{path}{sep}{n}"  # update path
+
+
 # Set printoptions
 torch.set_printoptions(linewidth=320, precision=5, profile='long')
 np.set_printoptions(linewidth=320, formatter={'float_kind': '{:11.5g}'.format})  # format short g, %precision=5
